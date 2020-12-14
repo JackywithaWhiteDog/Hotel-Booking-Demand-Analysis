c37cb6640267ae204f24553aa97f904e1a49bf00
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2020-12-14 15:09:39 +0800

    remove git hooks

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-git config core.hooksPath "$(pwd)/.githooks"
-
-pip install --upgrade pip && \
-( pip install --no-cache-dir -r requirements.txt  || \
-  pip3 install --no-cache-dir -r requirements.txt || \
-  python -m pip install --no-cache-dir -r requirements.txt  || \
+( pip install --upgrade pip                                   || \
+  pip3 install --upgrade pip                                  || \
+  python -m pip install --upgrade pip                         || \
+  python3 -m pip install --upgrade pip )                      && \
+( pip install --no-cache-dir -r requirements.txt              || \
+  pip3 install --no-cache-dir -r requirements.txt             || \
+  python -m pip install --no-cache-dir -r requirements.txt    || \
   python3 -m pip install --no-cache-dir -r requirements.txt ) && \
-( pip install --no-cache-dir -e HTML  || \
-  pip3 install --no-cache-dir -e HTML || \
-  python -m pip install --no-cache-dir -e HTML  || \
+( pip install --no-cache-dir -e HTML                          || \
+  pip3 install --no-cache-dir -e HTML                         || \
+  python -m pip install --no-cache-dir -e HTML                || \
   python3 -m pip install --no-cache-dir -e HTML )
 
 wget -c -i dataset.txt -P ./data
