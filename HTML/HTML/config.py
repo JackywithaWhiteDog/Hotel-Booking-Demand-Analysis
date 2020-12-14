b8f189215ff77dcd87688ae8fde31c3f7a7b2a9f
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2020-12-14 14:01:47 +0800

    refactor: remove unused code

--- HTML/HTML/config.py
+++ HTML/HTML/config.py
@@ -1,11 +1,3 @@
-# import os
-
-# data_dir = os.path.abspath('../../data')
-
-# test_path = os.path.join(data_dir, 'test.csv')
-# train_path = os.path.join(data_dir, 'train.csv')
-# train_label_path = os.path.join(data_dir, 'train_label.csv')
-
 from pathlib import Path
 
 data_dir = Path(__file__).parent / '../../data'
